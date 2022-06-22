7de2529c8bf58a1547540ad75281c7bf8d5afaf5
MistakeOfficial <kan0958300341@gmail.com>
2022-06-22 08:45:17 +0700

    first commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+dev: yarn start
+package: npm i captcha-canvas@3.1.0 discord.js@13.7.0
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  token: procces.env.TOKEN || "BOT_TOKEN",
+  guildId: procces.env.SVID || "GUILD_ID",
+  verifyRole: procces.env.VFY_ROLE"VERIFY_ROLE",
+  verifyChannel: procces.env.VFY_CH"VERIFY_CHANNEL",
+};
